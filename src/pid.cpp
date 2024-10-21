3ce9dc93381837561ddc6e16310919e6e9ef2122
Anish Viswanatham <102044149+RubyRayquaza@users.noreply.github.com>
2024-10-20 19:08:00 -0700

    Update pid.cpp

--- src/pid.cpp
+++ src/pid.cpp
@@ -1,6 +1,6 @@
 #include "pid.hpp"
 
-// Constructor implementation
+// Construction Implementation
 PID::PID(double kp, double ki, double kd)
     : kp(kp), ki(ki), kd(kd), integral(0), previous_error(0) {}
 
@@ -10,5 +10,5 @@ double PID::calculate(double setpoint, double actual) {
     double derivative = error - previous_error;
     previous_error = error;
 
-    return (kp * error) + (ki * integral) + (kd * derivative);
+    return kp * error + ki * integral + kd * derivative;
 }
