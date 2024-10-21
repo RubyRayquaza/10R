961f4dd15942f2e530f62d45fba2032a37110921
Anish Viswanatham <102044149+RubyRayquaza@users.noreply.github.com>
2024-10-20 18:06:09 -0700

    Create drivetrain.hpp

---
+++ src/drivetrain.hpp
@@ -0,0 +1,24 @@
+#ifndef DRIVETRAIN_HPP
+#define DRIVETRAIN_HPP
+
+#include "pros/motors.hpp"
+#include "lemlib/Drivetrain.hpp"
+
+class Drivetrain {
+public:
+    // Constructor
+    Drivetrain(pros::MotorGroup* leftMotors, pros::MotorGroup* rightMotors);
+
+    // Movement functions
+    void moveForward(int velocity);
+    void moveBackward(int velocity);
+    void turnLeft(int velocity);
+    void turnRight(int velocity);
+    void stop();
+
+private:
+    pros::MotorGroup* left_motors;   // Left motor group
+    pros::MotorGroup* right_motors;  // Right motor group
+};
+
+#endif // DRIVETRAIN_HPP
