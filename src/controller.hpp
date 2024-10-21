91ff874ec28250bc94d3852a8d5404dbc350c6a5
Anish Viswanatham <102044149+RubyRayquaza@users.noreply.github.com>
2024-10-20 18:22:58 -0700

    Update controller.hpp

--- src/controller.hpp
+++ src/controller.hpp
@@ -1,1 +1,16 @@
+#ifndef CONTROLLER_HPP
+#define CONTROLLER_HPP
 
+#include "pros/adi.hpp"
+#include "pros/motors.hpp"
+
+class Controller {
+public:
+    Controller(pros::Controller &controller);
+    void driveTank(int leftY, int rightY);
+
+private:
+    pros::Controller &controller;
+};
+
+#endif // CONTROLLER_HPP
