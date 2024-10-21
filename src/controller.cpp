8a2036e41c3a243f5776e15c2cf68e8b141a59de
Anish Viswanatham <102044149+RubyRayquaza@users.noreply.github.com>
2024-10-20 18:59:22 -0700

    Update controller.cpp

--- src/controller.cpp
+++ src/controller.cpp
@@ -1,14 +1,15 @@
 #include "controller.hpp"
 
-// Constructor implementation
-Controller::Controller(pros::E_CONTROLLER_ID id) : controller(id) {}
+Controller::Controller(pros::E_CONTROLLER_ID id, Drivetrain &drivetrain)
+    : controller(id), drivetrain(drivetrain) {}
 
-// Get left joystick Y value
-int Controller::getLeftY() {
-    return controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-}
+void Controller::driveTank() {
+    int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
+    int rightY = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
 
-// Get right joystick Y value
-int Controller::getRightY() {
-    return controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_Y);
+    if (leftY == 0 && rightY == 0) {
+        drivetrain.stop(); // Call stop method from the drivetrain
+    } else {
+        drivetrain.tank(leftY, rightY); // Use the drivetrain's tank method
+    }
 }
